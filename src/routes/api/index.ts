261eaff725417875ea8cc9919354cf5a5c7241cc
Jhinger <gurshanjhinger22@gmail.com>
2024-07-14 11:24:37 -0700

    min reproducible

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -5,13 +5,5 @@ import type { APIEvent } from "@solidjs/start/server";
 export function GET(event: APIEvent) {
   const url = new URL("https://github.com/Jhinger")
 
-	setCookie(event.nativeEvent, "github_oauth_state", "abc", {
-		path: "/",
-		secure: process.env.NODE_ENV === "production",
-		httpOnly: true,
-		maxAge: 60 * 10,
-		sameSite: "lax"
-	});
-
 	return sendRedirect(event.nativeEvent, url.toString());
 }
